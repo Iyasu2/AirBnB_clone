17d9d8473dbd875e62c0cf7778e5d0b513112ee9
Ab-Ruh <abell.demere@gmail.com>
2023-08-08 17:40:01 +0300

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module defines the
+BaseModel class that other
+classes can inherit from.
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    This is the BaseModel class that provides common attributes/methods.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the BaseModel instance.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Return a string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Update the 'updated_at' attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Return a dictionary containing all attributes of the instance.
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
